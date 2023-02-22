41244783ff5cae4d5e2c7f348f901ecdcac01a7a
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-22 15:53:42 +0100

    ....

---
+++ React/la-maison-jungle/src/components/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
+
+function App() {
+	const [cart, updateCart] = useState([])
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
+		</div>
+	)
+}
+
+export default App
